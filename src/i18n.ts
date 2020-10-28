167d64c8b72d5d7fcffb3deac4a9eab1169940a2
Тимофей Полухин <xalvaine@gmail.com>
2020-10-28 18:49:44 +0300

    chore: switch off i18n debug mode

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,7 +9,6 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     fallbackLng: `ru`,
-    debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
